b6f743564512015be19295dd15cb163b10a787e4
unknownplayer27 <126305902+1proprogrammerchant@users.noreply.github.com>
2023-06-03 09:11:09 -0500

    Create samplemodemclass.py

---
+++ samplemodemclass.py
@@ -0,0 +1,11 @@
+# Create an instance of the MachineModem class.
+machine_modem = MachineModem(ac_remote)
+
+# Connect to the AC.
+machine_modem.connect()
+
+# Do something with the AC.
+# ...
+
+# Disconnect from the AC.
+machine_modem.disconnect()
